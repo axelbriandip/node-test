308175edb4df8036e6c31ae9cdf8e5f5962153c2
axelbriandip <axelbriandip.rg@gmail.com>
2022-12-20 00:34:05 -0300

    asdasd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/players', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send({
         msg: 'hola mundo'
     })
